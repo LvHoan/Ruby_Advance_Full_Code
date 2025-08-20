ef909d4d18d5db076345d5ff06cb5f6d36346122
hoanlv <lehoan2742003@gmail.com>
2025-08-21 01:37:48 +0700

    fix comment 1

--- app/jobs/bulk_email_job.rb
+++ app/jobs/bulk_email_job.rb
@@ -4,15 +4,10 @@ class BulkEmailJob < ApplicationJob
   BATCH_SIZE = 100
 
   def perform(subject, content)
-    users = User.all
     title = EmailConstants::BULK_ANNOUNCEMENT_TITLE
 
-    # Lấy theo batch 100 user
-    users.find_in_batches(batch_size: BATCH_SIZE) do |batch|
-      batch.each do |user|
-        # enqueue riêng cho từng email, retry riêng từng user
-        Logic::SendSingleEmailJob.perform_later(user.id, subject, title, content)
-      end
+    User.find_each(batch_size: BATCH_SIZE) do |user|
+      Logic::SendSingleEmailJob.perform_later(user.id, subject, title, content)
     end
   end
 end
